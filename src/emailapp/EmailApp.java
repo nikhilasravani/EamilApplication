7b495123bab690f4647c1ef2010613ea62b1c417
nikhilasravani <46299218+nikhilasravani@users.noreply.github.com>
2023-05-07 01:05:57 +0530

    Update EmailApp.java

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -4,7 +4,22 @@ public class EmailApp {
 
 	public static void main(String[] args) {
 		Email em = new Email();
+		//Show the  details
 		em.showInfo();
+		
+		System.out.println("--------------------------------------------");
+		//change mailbox capacity
+		em.setMaxEmailCount(900);
+		System.out.println("Updated mailbox Capacity: "+em.getMaxEmailCount());
+		
+		//change Password
+		em.changePassword("GH^m4n$3");
+		System.out.println("New Password: "+em.getPassword());
+		
+		//add alternate email ID
+		em.setAlternateEmail("jsk@company.com");
+		System.out.println("Alternate Email ID: "+em.getAlternateEmail());
+		
 
 	}
 
